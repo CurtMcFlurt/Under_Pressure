4d000834b8b1c8bcaf0ae2a6a4b001fcf0b819a9
Snajken <a22elidr@student.his.se>
2025-05-15 14:06:22 +0200

    puzzle now works

--- Assets/Scripts/Interactables/StartPuzzleInteract.cs
+++ Assets/Scripts/Interactables/StartPuzzleInteract.cs
@@ -6,19 +6,27 @@ public class StartPuzzleInteract : Interactable
     public TakeTheCamera myCameraNow;
     public GameObject CameraPoint;
     public Collider interactCollider;
+    public GameObject lidObject;
+    public CogPuzzleManager manager;
+    public float lidRotation = 90f; // Desired rotation in degrees
+    public float lidLerpSpeed = 2f; // How fast the lid opens/closes
+    public bool lockMe;
+   
 
     public void OnEnable()
     {
         interactCollider = GetComponent<Collider>();
     }
+
     public override void StartInteraction(GameObject sender)
     {
+        if (lockMe) { return; }
         var take = sender.GetComponent<TakeTheCamera>();
         myCameraNow = take;
         take.activeStealer = this;
         take.StealThecamera(CameraPoint);
 
-        taken= true;
+        taken = true;
     }
 
     public void Update()
@@ -26,10 +34,51 @@ public class StartPuzzleInteract : Interactable
         if (taken)
         {
             interactCollider.enabled = false;
+
+            if (lidObject != null)
+            {
+                Quaternion targetRotation = Quaternion.Euler(0, 0, -lidRotation);
+                lidObject.transform.localRotation = Quaternion.Lerp(
+                    lidObject.transform.localRotation,
+                    targetRotation,
+                    Time.deltaTime * lidLerpSpeed
+                );
+            }
         }
-        else { 
+        else
+        {
             interactCollider.enabled = true;
             myCameraNow = null;
+
+            if (lidObject != null)
+            {
+                Quaternion targetRotation = Quaternion.Euler(0, 0, 0);
+                lidObject.transform.localRotation = Quaternion.Lerp(
+                    lidObject.transform.localRotation,
+                    targetRotation,
+                    Time.deltaTime * lidLerpSpeed
+                );
+            }
         }
+    }
+    public void UnPackData(Component sender, object data)
+    {
+        // Check if the data is a string before proceeding
+        if (data is string keyName)
+        {
+            Debug.Log($"Scene change requested: {keyName}");
+
+            if (keyName == manager.sendString) { taken = false; lockMe = true; }
+        }
+        else
+        {
+            Debug.LogWarning("Data is not a valid scene name string.");
+        }
+
+        
+    }
+    public void ReleasePuzzle()
+    {
+        taken = false;
     }
 }
--- Assets/Scripts/PuzzleSpecifics/CogPuzzleManager.cs
+++ Assets/Scripts/PuzzleSpecifics/CogPuzzleManager.cs
@@ -1,9 +1,8 @@
-using NUnit.Framework;
-using System.Collections.Generic;
-using System.Linq;
+using Unity.Netcode;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class CogPuzzleManager : MonoBehaviour
+public class CogPuzzleManager : NetworkBehaviour
 {
     public List<Cog> myCogs = new List<Cog>();
     public GameObject cogParent;
@@ -12,7 +11,13 @@ public class CogPuzzleManager : MonoBehaviour
     public List<SocketInteracting> mySocks = new List<SocketInteracting>();
     public GameEvent winEvent;
     public string sendString;
-    void OnEnable()
+
+    public NetworkVariable<bool> puzzleSolved = new NetworkVariable<bool>(
+        false,
+        NetworkVariableReadPermission.Everyone
+    );
+
+    private void OnEnable()
     {
         myCollider = GetComponent<Collider>();
         myCogs.Clear();
@@ -20,30 +25,32 @@ public class CogPuzzleManager : MonoBehaviour
         foreach (Transform child in cogParent.transform)
         {
             Cog cog = child.GetComponent<Cog>();
-            if (cog != null&& !myCogs.Contains(cog))
+            if (cog != null && !myCogs.Contains(cog))
             {
                 myCogs.Add(cog);
             }
         }
+
+        mySocks.Clear();
         foreach (Transform child in sockParent.transform)
         {
             foreach (Transform child2 in child)
             {
-
-
                 SocketInteracting cog = child2.GetComponent<SocketInteracting>();
                 if (cog != null && !mySocks.Contains(cog))
                 {
                     mySocks.Add(cog);
                 }
             }
-
-            
         }
+
     }
+
+    private bool notWon;
 
     void FixedUpdate()
     {
+
         foreach (var cog in myCogs)
         {
             if (!myCollider.bounds.Contains(cog.transform.position))
@@ -52,22 +59,32 @@ public class CogPuzzleManager : MonoBehaviour
                 Debug.Log("getMyCog");
             }
         }
+
         int totalSolved = 0;
         for (int i = 0; i < mySocks.Count; i++)
         {
             if (mySocks[i].Correct) totalSolved++;
+            Debug.Log(totalSolved + " ammount solved");
         }
 
-        if(totalSolved>=mySocks.Count && totalSolved != 0)
+        if (totalSolved >= mySocks.Count && totalSolved != 0)
+        {
+            puzzleSolved.Value = true; // This will trigger OnPuzzleSolvedChanged on all clients
+            Debug.Log("puzzle should be solved");
+        }
+
+        if (puzzleSolved.Value && !notWon)
         {
             SolvedPuzzle();
         }
     }
 
+  
 
     public void SolvedPuzzle()
     {
-        Debug.Log("Solved:" + sendString);
+        Debug.Log("Solved: " + sendString);
         winEvent.Raise(this, sendString);
+        notWon = true;
     }
 }
